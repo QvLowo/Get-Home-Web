a0872e761a32e3acf43ad03ae4b78ae55b0e0d2b
QvL <qvlywk@gmail.com>
2024-05-01 00:03:57 +0800

    initial Spring Boot project

---
+++ GetHomeWeb/src/test/java/com/qvl/gethomeweb/GetHomeWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qvl.gethomeweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetHomeWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
