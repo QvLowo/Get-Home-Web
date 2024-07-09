c022596c2b51374336330bd24744d27547cb3ba5
qul <qvlywk@gmail.com>
2024-07-09 12:04:41 +0800

    單元測試相關
    (cherry picked from commit decf75835635c7265180ad8704240965e83ba2c6)

--- GetHomeWeb/src/test/resources/data.sql
+++ GetHomeWeb/src/test/resources/data.sql
@@ -1,10 +1,9 @@
-INSERT INTO House (house_type, address, image_url, price, utilities, square, management_cost, gender, status, description) VALUES ('BEDSIT','桃園市楊梅區新富十一街30號','https://reurl.cc/9v1l7a',5000,1000,7,100,'BOTH','AVAILABLE','');
-INSERT INTO House (house_type, address, image_url, price, utilities, square, management_cost, gender, status, description)VALUES('STUDIO','宜蘭縣冬山鄉龍祥九路3號','https://reurl.cc/EjXAD0',10000,1500,12,100,'FEMALE','RESERVED','');
-INSERT INTO House (house_type, address, image_url, price, utilities, square, management_cost, gender, status, description)VALUES('STUDIO','高雄市前鎮區瑞恩街18號','https://reurl.cc/xa1RoV',15000,4000,15,200,'MALE','AVAILABLE','');
-INSERT INTO House (house_type, address, image_url, price, utilities, square, management_cost, gender, status, description)VALUES('APARTMENT','臺南市鹽水區四維路8號','https://reurl.cc/DjX0x6',20000,3000,24,1000,'BOTH','AVAILABLE','');
-INSERT INTO House (house_type, address, image_url, price, utilities, square, management_cost, gender, status, description)VALUES('BEDSIT','花蓮縣吉安鄉莊敬路43號','https://reurl.cc/KeXG5g',4000,1000,9,100,'BOTH','RENTED','');
-INSERT INTO House (house_type, address, image_url, price, utilities, square, management_cost, gender, status, description)VALUES('SHAREROOMS','臺南市白河區外角18號','https://reurl.cc/bVGvAM',7000,500,10,150,'BOTH','AVAILABLE','');
-INSERT INTO House (house_type, address, image_url, price, utilities, square, management_cost, gender, status, description)VALUES('APARTMENT','台中市中區民族路130號','https://reurl.cc/kOq6gn',30000,1200,30,1500,'FEMALE','AVAILABLE','');
-INSERT INTO House (house_type, address, image_url, price, utilities, square, management_cost, gender, status, description)VALUES('SHAREROOMS','臺北市南港區三重路24號','https://reurl.cc/ezWr7K',25000,2000,10,500,'MALE','RESERVED','');
-INSERT INTO House (house_type, address, image_url, price, utilities, square, management_cost, gender, status, description)VALUES('BEDSIT','南投縣南投市學府九街24號','https://reurl.cc/RqON79',9000,800,8,100,'BOTH','AVAILABLE','');
-INSERT INTO House (house_type, address, image_url, price, utilities, square, management_cost, gender, status, description)VALUES('BEDSIT','新北市板橋區三民路二段30號','https://reurl.cc/z1rV4y',18000,1400,10,200,'FEMALE','AVAILABLE','');
+INSERT INTO house (user_id, house_name, house_type, address, image_url, price_per_month, status, description, created_date, last_update_date) VALUES (1, '星光大廈', 'APARTMENT', '新北市板橋區海山路15號', 'https://images.pexels.com/photos/19799232/pexels-photo-19799232.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2', 5000, 'AVAILABLE', '', '2024-06-24', '2024-06-24');
+
+INSERT INTO house (user_id, house_name, house_type, address, image_url, price_per_month, status, description, created_date, last_update_date) VALUES (1, '藍天名苑', 'APARTMENT', '新北市中和區成功路15號', 'https://images.pexels.com/photos/210265/pexels-photo-210265.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2', 10000, 'AVAILABLE', '', '2024-06-24', '2024-06-24');
+
+INSERT INTO house (user_id, house_name, house_type, address, image_url, price_per_month, status, description, created_date, last_update_date) VALUES (1, '玫瑰花園', 'STUDIO', '台北市中正區寧波西街3號', 'https://images.pexels.com/photos/1669799/pexels-photo-1669799.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2', 15000, 'AVAILABLE', '', '2024-06-24', '2024-06-24');
+
+INSERT INTO house (user_id, house_name, house_type, address, image_url, price_per_month, status, description, created_date, last_update_date) VALUES (1, '陽光廣場', 'APARTMENT', '台北市士林區至誠路9號', 'https://images.pexels.com/photos/259962/pexels-photo-259962.jpeg', 20000, 'AVAILABLE', '', '2024-06-24', '2024-06-24');
+
+INSERT INTO house (user_id, house_name, house_type, address, image_url, price_per_month, status, description, created_date, last_update_date) VALUES (1, '綠意新城', 'BEDSIT', '台北市中正區寧波西街3號', 'https://images.pexels.com/photos/271619/pexels-photo-271619.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2', 4000, 'AVAILABLE', '', '2024-06-24', '2024-06-24');
